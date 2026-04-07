29838d3111a28ea818291b88e1ce8776b7dd3150
Balaganesh V <balaganesh1016@gmail.com>
2026-04-07 14:58:44 +0530

    Fix Emscripten preload filesystem race condition via lazy initialization

--- ClinicRegister/wasm_bindings.cpp
+++ ClinicRegister/wasm_bindings.cpp
@@ -1,8 +1,15 @@
 #include <emscripten.h>
 #include "HospitalSystem.h"
 
-// Instantiate the global application instance purely for the web interface
-HospitalSystem system_instance;
+// Instantiate lazily so Emscripten has time to mount preload files!
+HospitalSystem* system_ptr = nullptr;
+
+HospitalSystem& getSystem() {
+    if (!system_ptr) {
+        system_ptr = new HospitalSystem();
+    }
+    return *system_ptr;
+}
 
 extern "C" {
     
@@ -10,32 +17,32 @@ extern "C" {
     
     EMSCRIPTEN_KEEPALIVE
     bool patientExists_js(const char* id) {
-        return system_instance.patientExists(id);
+        return getSystem().patientExists(id);
     }
     
     EMSCRIPTEN_KEEPALIVE
     void registerPatient_js(const char* id, const char* name, int age, const char* phone) {
-        system_instance.registerPatient(id, name, age, phone);
+        getSystem().registerPatient(id, name, age, phone);
     }
 
     EMSCRIPTEN_KEEPALIVE
     void addVisit_js(const char* patientID, const char* date, const char* diagnosis, const char* prescription) {
-        system_instance.addVisit(patientID, date, diagnosis, prescription);
+        getSystem().addVisit(patientID, date, diagnosis, prescription);
     }
     
     EMSCRIPTEN_KEEPALIVE
     void displayPatientHistory_js(const char* patientID) {
-        system_instance.displayPatientHistory(patientID);
+        getSystem().displayPatientHistory(patientID);
     }
     
     EMSCRIPTEN_KEEPALIVE
     void reportFrequentVisitors_js(int n) {
-        system_instance.reportFrequentVisitors(n);
+        getSystem().reportFrequentVisitors(n);
     }
 
     EMSCRIPTEN_KEEPALIVE
     void reportTotalVisitsThisMonth_js(const char* month) {
-        system_instance.reportTotalVisitsThisMonth(month);
+        getSystem().reportTotalVisitsThisMonth(month);
     }
 
 }
